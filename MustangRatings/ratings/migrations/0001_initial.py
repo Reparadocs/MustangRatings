7685a0dc15cbac9071d1eead6215dbb638a9e5a5
Rishab Hegde <reparadocs@pcp066544pcs.wireless.calpoly.edu>
2015-02-21 12:18:47 -0800

    Fixing Shit

---
+++ MustangRatings/ratings/migrations/0001_initial.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='CPairing',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Major',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=30)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='MClass',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('number', models.IntegerField()),
+                ('major', models.ForeignKey(to='ratings.Major')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Professor',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=100)),
+                ('polyrating', models.CharField(max_length=10)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Rating',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('rating', models.FloatField()),
+                ('cpairing', models.ForeignKey(to='ratings.CPairing')),
+                ('major', models.ForeignKey(to='ratings.Major')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='UserWrapper',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('owner', models.OneToOneField(to=settings.AUTH_USER_MODEL)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='rating',
+            name='owner',
+            field=models.ForeignKey(to='ratings.UserWrapper'),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='cpairing',
+            name='mclass',
+            field=models.ForeignKey(to='ratings.MClass'),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='cpairing',
+            name='professor',
+            field=models.ForeignKey(to='ratings.Professor'),
+            preserve_default=True,
+        ),
+    ]
--- MustangRatings/ratings/models.py
+++ MustangRatings/ratings/models.py
@@ -18,7 +18,7 @@ class CPairing(models.Model):
 
    def getAverage():
       total = 0.0
-      for rating in self.rating_set.all():
+      for rating in self.rating_set:
          total += rating.rating
 
 class UserWrapper(models.Model):
@@ -28,7 +28,7 @@ class UserWrapper(models.Model):
       total = 0.0
       stotal = 0.0
       num = 0
-      for rating in self.rating_set.all():
+      for rating in self.rating_set:
          if rating.major is major:
             num += 1
             total += rating.rating
@@ -41,7 +41,7 @@ class UserWrapper(models.Model):
       total = 0.0
       stotal = 0.0
       num = len(self.rating_set.all())
-      for rating in self.rating_set.all()
+      for rating in self.rating_set:
          total += rating.rating
          stotal += rating.cpairing.average
       ave1 = total/num
