86f22cef993ebb92ad18d3f70f3b121229f56432
MitchellZehr <mitchellandandrew@hotmail.com>
2015-02-21 13:54:41 -0800

    view thing

--- MustangRatings/MustangRatings/ratings/views.py
+++ MustangRatings/MustangRatings/ratings/views.py
@@ -7,20 +7,19 @@ def main(request):
       if request.method == 'GET':
 	     majorList = Major.objects.all()
 		 mForm = MajorForm();
-		 return render(request, {'form' : mForm})
+		 return render(request, {'form' : mForm, 'majorList' : majorList})
 	  elif request.method == 'POST':
 	     
-		 return redirect('list', args=(request,profList))
+		 return redirect('list', args=(request,classID))
 
 	else #needs to log on	 
 		 
 		 
 def list(request, classID):
    classList = Class.objects.all()
-   for Class in classList:
-      if Class.ID == classID:
-	     professors = Class.getProfessors()
-		 return render(request, {'professors', professors})
+   c = MClass.objects.get(id=classID)
+   professors = c.getProfessors()
+   return render(request, {'professors', professors})
 	
 	
    #find class from classID
