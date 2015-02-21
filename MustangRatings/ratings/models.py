1a253b2474d113111d3536210749d7329ed53c10
Faraaz Nishtar <faraaznishtar@gmail.com>
2015-02-21 14:03:25 -0800

    routes working

--- MustangRatings/ratings/models.py
+++ MustangRatings/ratings/models.py
@@ -29,7 +29,6 @@ class CPairing(models.Model):
    professor = models.ForeignKey(Professor)
    mclass = models.ForeignKey(MClass)
 
-<<<<<<< HEAD
    def getYourAverage(self, user):
       ave = self.getAverage()
       majorAve = user.getMajorAverage()
@@ -38,9 +37,6 @@ class CPairing(models.Model):
       return ave+userAve
 
    def getAverage(self):
-=======
-   def getgAverage():
->>>>>>> f3683c0a0834c5e4f92fa972fe2faeb361e92141
       total = 0.0
       for rating in self.rating_set:
          total += rating.rating
